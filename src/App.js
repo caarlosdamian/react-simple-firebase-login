6be7bcd6b1f0251c5d53d5f4f75111538106dff3
caarlosdamian <dapecarlos@gmail.com>
2021-08-23 09:55:02 -0500

    Feature: Add Firebase-React-router-dom, add Firebase config file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./sections/Home";
+import Singup from "./components/Singup";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/singup" component={Singup} />
+      </div>
+    </Router>
   );
 }
 
